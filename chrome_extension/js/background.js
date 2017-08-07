44d1c60cda20263af0c33b4f14b27899b5eb7dfa
Etan Zapinsky <etan.zapinsky@gmail.com>
2017-08-06 21:20:38 -0400

    Use promises to inject scripts for readability.

--- chrome_extension/js/background.js
+++ chrome_extension/js/background.js
@@ -2,23 +2,33 @@ var toggle = false;
 chrome.browserAction.onClicked.addListener(function(tab) {
         toggle = !toggle;
         if(toggle){
-                chrome.browserAction.setIcon({path: "icons/on.png", tabId: tab.id});
-                chrome.tabs.executeScript(tab.id, {file:"js/jquery-3.2.1.min.js"}, function() {
-                        chrome.tabs.executeScript(tab.id, {file:"js/underscore-1.8.3.min.js"}, function() {
-                                chrome.tabs.executeScript(tab.id, {file:"js/lib.js"}, function() {
-                                        chrome.tabs.executeScript(tab.id, {file:"js/encrypt.js"});
-});
-                        });
+                var promise = new Promise(function(resolve, reject) {
+                        chrome.browserAction.setIcon({path: "icons/on.png", tabId: tab.id}, resolve);
+                }).then(function(resolve, reject) {
+                        chrome.tabs.executeScript(tab.id, {file:"js/jquery-3.2.1.min.js"}, resolve);
+                }).then(function(resolve, reject) {
+                        chrome.tabs.executeScript(tab.id, {file:"js/underscore-1.8.3.min.js"}, resolve);
+                }).then(function(resolve, reject) {
+                        chrome.tabs.executeScript(tab.id, {file:"js/grapheme-splitter-1.0.1.js"}, resolve);
+                }).then(function(resolve, reject) {
+                        chrome.tabs.executeScript(tab.id, {file:"js/lib.js"}, resolve);
+                }).then(function(resolve, reject) {
+                        chrome.tabs.executeScript(tab.id, {file:"js/encrypt.js"}, resolve);
                 });
         }
         else{
-                chrome.browserAction.setIcon({path: "icons/off.png", tabId: tab.id});
-                chrome.tabs.executeScript(tab.id, {file:"js/jquery-3.2.1.min.js"}, function() {
-                        chrome.tabs.executeScript(tab.id, {file:"js/underscore-1.8.3.min.js"}, function() {
-                                chrome.tabs.executeScript(tab.id, {file:"js/lib.js"}, function() {
-                                        chrome.tabs.executeScript(tab.id, {file:"js/decrypt.js"});
-});
-                        });
+                var promise = new Promise(function(resolve, reject) {
+                        chrome.browserAction.setIcon({path: "icons/off.png", tabId: tab.id}, resolve);
+                }).then(function(resolve, reject) {
+                        chrome.tabs.executeScript(tab.id, {file:"js/jquery-3.2.1.min.js"}, resolve);
+                }).then(function(resolve, reject) {
+                        chrome.tabs.executeScript(tab.id, {file:"js/underscore-1.8.3.min.js"}, resolve);
+                }).then(function(resolve, reject) {
+                        chrome.tabs.executeScript(tab.id, {file:"js/grapheme-splitter-1.0.1.js"}, resolve);
+                }).then(function(resolve, reject) {
+                        chrome.tabs.executeScript(tab.id, {file:"js/lib.js"}, resolve);
+                }).then(function(resolve, reject) {
+                        chrome.tabs.executeScript(tab.id, {file:"js/decrypt.js"}, resolve);
                 });
         }
 });

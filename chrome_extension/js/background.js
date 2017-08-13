2bbdc6b240944e70ec7bb82c64069e218b408cb6
Etan Zapinsky <etan.zapinsky@gmail.com>
2017-08-13 12:51:16 -0400

    WIP: obfuscation on working with state saved across tabs.

--- chrome_extension/js/background.js
+++ chrome_extension/js/background.js
@@ -1,34 +1,67 @@
-var toggle = false;
+function handleUnicodeObfuscationStart(tab) {
+        chrome.storage.local.get({'OBFUSCATION_ON': false}, function(object) {
+                var on = object.OBFUSCATION_ON;
+
+                if(on){
+                        var promise = new Promise(function(resolve, reject) {
+                                chrome.browserAction.setIcon({path: "icons/on.png"}, resolve);
+                        }).then(function() {
+                                chrome.tabs.executeScript(tab.id, {file:"js/jquery-3.2.1.min.js"});
+                        }).then(function() {
+                                chrome.tabs.executeScript(tab.id, {file:"js/underscore-1.8.3.min.js"});
+                        }).then(function() {
+                                chrome.tabs.executeScript(tab.id, {file:"js/grapheme-splitter-1.0.1.js"});
+                        }).then(function() {
+                                chrome.tabs.executeScript(tab.id, {file:"js/lib.js"});
+                        }).then(function() {
+                                chrome.tabs.executeScript(tab.id, {file:"js/encrypt.js"});
+                        });
+                }
+                else{
+                        var promise = new Promise(function(resolve, reject) {
+                                chrome.browserAction.setIcon({path: "icons/off.png"}, resolve);
+                        }).then(function() {
+                                chrome.tabs.executeScript(tab.id, {file:"js/jquery-3.2.1.min.js"});
+                        }).then(function() {
+                                chrome.tabs.executeScript(tab.id, {file:"js/underscore-1.8.3.min.js"});
+                        }).then(function() {
+                                chrome.tabs.executeScript(tab.id, {file:"js/grapheme-splitter-1.0.1.js"});
+                        }).then(function() {
+                                chrome.tabs.executeScript(tab.id, {file:"js/lib.js"});
+                        }).then(function() {
+                                chrome.tabs.executeScript(tab.id, {file:"js/decrypt.js"});
+                        });
+                }
+        });
+}
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-        toggle = !toggle;
-        if(toggle){
-                var promise = new Promise(function(resolve, reject) {
-                        chrome.browserAction.setIcon({path: "icons/on.png", tabId: tab.id}, resolve);
-                }).then(function(resolve, reject) {
-                        chrome.tabs.executeScript(tab.id, {file:"js/jquery-3.2.1.min.js"}, resolve);
-                }).then(function(resolve, reject) {
-                        chrome.tabs.executeScript(tab.id, {file:"js/underscore-1.8.3.min.js"}, resolve);
-                }).then(function(resolve, reject) {
-                        chrome.tabs.executeScript(tab.id, {file:"js/grapheme-splitter-1.0.1.js"}, resolve);
-                }).then(function(resolve, reject) {
-                        chrome.tabs.executeScript(tab.id, {file:"js/lib.js"}, resolve);
-                }).then(function(resolve, reject) {
-                        chrome.tabs.executeScript(tab.id, {file:"js/encrypt.js"}, resolve);
+        var promise = new Promise(function(resolve, reject) {
+                chrome.storage.local.get({'OBFUSCATION_ON': false}, function(object) {
+                        var on = object.OBFUSCATION_ON;
+                        
+                        resolve(on);
                 });
-        }
-        else{
-                var promise = new Promise(function(resolve, reject) {
-                        chrome.browserAction.setIcon({path: "icons/off.png", tabId: tab.id}, resolve);
-                }).then(function(resolve, reject) {
-                        chrome.tabs.executeScript(tab.id, {file:"js/jquery-3.2.1.min.js"}, resolve);
-                }).then(function(resolve, reject) {
-                        chrome.tabs.executeScript(tab.id, {file:"js/underscore-1.8.3.min.js"}, resolve);
-                }).then(function(resolve, reject) {
-                        chrome.tabs.executeScript(tab.id, {file:"js/grapheme-splitter-1.0.1.js"}, resolve);
-                }).then(function(resolve, reject) {
-                        chrome.tabs.executeScript(tab.id, {file:"js/lib.js"}, resolve);
-                }).then(function(resolve, reject) {
-                        chrome.tabs.executeScript(tab.id, {file:"js/decrypt.js"}, resolve);
+        }).then(function(on) {
+                // toggle on
+                on = !on
+
+                chrome.storage.local.set({'OBFUSCATION_ON': on}, function() {
+                        handleUnicodeObfuscationStart(tab);
                 });
+        });
+});
+
+chrome.tabs.onActivated.addListener(function(object) {
+                chrome.tabs.get(object.tabId, function(tab) {
+                handleUnicodeObfuscationStart(tab);
+        });
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+        var url = new URL(tab.url);
+
+        if (url.host.includes('twitter.com')) {
+                handleUnicodeObfuscationStart(tab);
         }
 });

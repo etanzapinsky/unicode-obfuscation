1e54043e4625ddfd6c56b0f803943b20a5dd92f2
Etan Zapinsky <etan.zapinsky@gmail.com>
2017-08-14 22:41:46 -0400

    Wrap injected code to check if function exists to prevent on double injection.

--- chrome_extension/js/twitter.js
+++ chrome_extension/js/twitter.js
@@ -1,69 +1,75 @@
-(function() {
-        var element = $('#tweet-box-home-timeline');
+// Only inject this function on the page if it doesn't exist yet
+if (typeof obfuscate != 'function') {
+        function obfuscate() {
+                var element = $('#tweet-box-home-timeline');
 
-        function processEncrypt() {
-                var clonedElement = element.clone();
+                function processEncrypt() {
+                        var clonedElement = element.clone();
 
-                // preprocess element's emoji symbols so cloned element has actual unicode
-                // value, not weird html spans
-                var richEmojis = clonedElement.find('span.RichEditor-pictographText');
-                _.each(richEmojis, function(emoji) {
-                        var unicode = $(emoji).attr('data-pictograph-text');
-                        $(emoji).text(unicode);
+                        // preprocess element's emoji symbols so cloned element has actual unicode
+                        // value, not weird html spans
+                        var richEmojis = clonedElement.find('span.RichEditor-pictographText');
+                        _.each(richEmojis, function(emoji) {
+                                var unicode = $(emoji).attr('data-pictograph-text');
+                                $(emoji).text(unicode);
+                        });
+
+                        var input = clonedElement.text();
+                        element.text(unicodeEncrypt(input));
+
+                        var range = document.createRange();
+                        range.selectNodeContents(element[0]);
+                        range.collapse(false);
+
+                        var sel = window.getSelection();
+                        sel.removeAllRanges();
+                        sel.addRange(range);
+                }
+
+                function processDecrypt() {
+                        var clonedElement = element.clone();
+
+                        // preprocess element's emoji symbols so cloned element has actual unicode
+                        // value, not weird html spans
+                        var richEmojis = clonedElement.find('span.RichEditor-pictographText');
+                        _.each(richEmojis, function(emoji) {
+                                var unicode = $(emoji).attr('data-pictograph-text');
+                                $(emoji).text(unicode);
+                        });
+
+                        var input = clonedElement.text();
+                        var decrypt = unicodeDecrypt(input);
+                        element.text(decrypt);
+
+                        var range = document.createRange();
+                        range.selectNodeContents(element[0]);
+                        range.collapse(false);
+
+                        var sel = window.getSelection();
+                        sel.removeAllRanges();
+                        sel.addRange(range);
+                }
+
+                var obfuscationOn;
+                chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+                        obfuscationOn = message.obfuscationOn;
+
+                        if (obfuscationOn) {
+                                processEncrypt();
+                        } else {
+                                processDecrypt();
+                        }
                 });
 
-                var input = clonedElement.text();
-                element.text(unicodeEncrypt(input));
 
-                var range = document.createRange();
-                range.selectNodeContents(element[0]);
-                range.collapse(false);
-                var sel = window.getSelection();
-                sel.removeAllRanges();
-                sel.addRange(range);
-        }
-
-        function processDecrypt() {
-                var clonedElement = element.clone();
-
-                // preprocess element's emoji symbols so cloned element has actual unicode
-                // value, not weird html spans
-                var richEmojis = clonedElement.find('span.RichEditor-pictographText');
-                _.each(richEmojis, function(emoji) {
-                        var unicode = $(emoji).attr('data-pictograph-text');
-                        $(emoji).text(unicode);
+                element.on('input', function(event) {
+                        if (obfuscationOn) {
+                                processEncrypt();
+                        } else {
+                                processDecrypt();
+                        }
                 });
-
-                // this input is definitely in local storage since this is the base
-                var input = clonedElement.text();
-                var decrypt = unicodeDecrypt(input);
-                element.text(decrypt);
-
-                var range = document.createRange();
-                range.selectNodeContents(element[0]);
-                range.collapse(false);
-                var sel = window.getSelection();
-                sel.removeAllRanges();
-                sel.addRange(range);
         }
 
-        var obfuscationOn;
-        chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-                obfuscationOn = message.obfuscationOn;
-
-                if (obfuscationOn) {
-                        processEncrypt();
-                } else {
-                        processDecrypt();
-                }
-        });
-
-
-        element.on('input', function(event) {
-                if (obfuscationOn) {
-                        processEncrypt();
-                } else {
-                        processDecrypt();
-                }
-        });
-})();
+        obfuscate();
+}
